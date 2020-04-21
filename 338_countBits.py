642f25a66eb5de6ec1cf41a27f6068133147cc0c
gotogoal <314104490@qq.com>
2020-04-21 10:04:27 +0800

    countBits addjust

--- 338_countBits.py
+++ 338_countBits.py
@@ -5,14 +5,17 @@ class Solution:
     def countBits(self, num):
         if num == 0:
             return [0]
+
         elif num == 1:
-            return [0, 1
-                    ]
+            return [0, 1]
+
         result = [0, 1]
         for i in range(2, num + 1):
             if i % 2 == 0:
+                # 当前数是偶数的话，与i / 2的1的个数一致
                 result.append(result[i//2])
             else:
+                # 当前数是奇数的话，比i / 2的1的个数多1
                 result.append(result[i//2]+1)
 
         return result
