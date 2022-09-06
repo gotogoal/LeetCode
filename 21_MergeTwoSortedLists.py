c87ade34f27493eea617f9a5c0d346bfb4b269d5
yinhe <yinhe@autohome.com.cn>
2022-09-06 08:02:36 +0800

    add 注释

---
+++ 21_MergeTwoSortedLists.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     :       2022/9/6 7:59
+# @Author   :       YH
+# @FILE     :       21_MergeTwoSortedLists.py
+# @Software :       PyCharm
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head = ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+                current = current.next
+            else:
+                current.next = list2
+                list2 = list2.next
+                current = current.next
+
+        current.next = list1 if list1 else list2
+
+        return head.next
